42e9ce5af19e799c37ef3de0bb397fd4ba79bae1
torsinclair <tor@romanesco.se>
2016-03-19 15:17:44 -0700

    implement search feature

--- client/components/Search.js
+++ client/components/Search.js
@@ -14,11 +14,37 @@ class Search extends React.Component {
     return (
       <div className="search-bar form-inline">
         <input className="form-control" type="text" />
-        <button className="btn hidden-sm-down">
+        <button className="btn hidden-sm-down" onClick={this.handleClick.bind(this)}>
           <span className="glyphicon glyphicon-search"></span>
         </button>
       </div> 
     );
+  }
+
+  handleClick() {
+    var options = {
+      part: 'snippet',
+      query: $('.form-control').val(),
+      max: 5,
+      key: YOUTUBE_API_KEY
+    };
+    searchYouTube(options, this.setData.bind(this));
+  }
+
+  setData(data) {
+    var video;
+
+    exampleVideoData = [];
+    data.items.forEach(function(datum) {
+      video = {};
+      video.kind = datum.kind;
+      video.etag = datum.etag;
+      video.id = datum.id;
+      video.snippet = datum.snippet;
+      exampleVideoData.push(video);
+    });
+
+    console.log(exampleVideoData);
   }
 }
 
--- client/lib/searchYouTube.js
+++ client/lib/searchYouTube.js
@@ -1,5 +1,19 @@
-var searchYouTube = (options, callback) => {
+var searchYouTube = (options, callback) => { 
   // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    contentType: 'application/json',
+    data: options,
+    success: function(data) {
+      console.log('SUCCESS!!');
+      callback(data);
+    },
+    error: function(data) {
+      console.log('ERROR!!');
+    }
+  });
+
 };
 
 window.searchYouTube = searchYouTube;
